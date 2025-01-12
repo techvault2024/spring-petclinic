1740524c96fb4a9c034395c8d9b3800a07f6a5cf
techvault2024 <techvault2024@outlook.com>
2025-01-12 11:41:25 -0500

    Updated VetTests.java
    break test

--- src/test/java/org/springframework/samples/petclinic/vet/VetTests.java
+++ src/test/java/org/springframework/samples/petclinic/vet/VetTests.java
@@ -33,7 +33,8 @@ class VetTests {
 		vet.setId(123);
 		@SuppressWarnings("deprecation")
 		Vet other = (Vet) SerializationUtils.deserialize(SerializationUtils.serialize(vet));
-		assertThat(other.getFirstName()).isEqualTo(vet.getFirstName());
+		#assertThat(other.getFirstName()).isEqualTo(vet.getFirstName());
+		assertThat(other.getFirstName()).isEqualTo(foo);
 		assertThat(other.getLastName()).isEqualTo(vet.getLastName());
 		assertThat(other.getId()).isEqualTo(vet.getId());
 	}
